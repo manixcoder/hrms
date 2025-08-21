dcf1ff47a83ea76d367c931a029dfc528c8cb78c
Manish Kumar Pathak <pathakmanish86@gmail.com>
2025-08-22 00:59:21 +0530

    Category add and listing

--- HRMS/src/Components/Category.jsx
+++ HRMS/src/Components/Category.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const Category = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        // Fetch categories from the backend if needed
+        axios.get('http://localhost:3000/auth/category')
+            .then(response => {
+                console.log(response.data);
+                if (response.data.Status) {
+                    setCategories(response.data.categories);
+                } else {
+                    console.error('Error fetching categories:', response.data.message);
+                }
+            })
+            .catch(error => console.error('There was an error fetching the categories!', error));
+
+    }, []);
+
+
     return (
         <div className='px-5 mt-3'>
             <div className='d-flex justify-content-center'>
@@ -10,6 +30,27 @@ const Category = () => {
             <Link to="/dashboard/category/add" className='btn btn-primary mb-3'>
                 <i className="bi bi-plus-circle"></i> Add Category
             </Link>
+            <div className='mt-3'>
+                <table className='table table-bordered table-striped'>
+                    <thead className='table'>
+                        <tr>
+                            <th>Name</th>
+                        </tr>
+                    </thead>
+                    <tbody className='table-light'>
+                        {categories.map((category, index) => (
+                            <tr key={index}>
+                                <td>{category.name}</td>
+                            </tr>
+                        ))}
+                        {categories.length === 0 && (
+                            <tr>
+                                <td colSpan="1" className='text-center'>No categories found</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
